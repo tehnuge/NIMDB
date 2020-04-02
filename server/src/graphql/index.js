b776fac5debc83a4a416df3bdc3a5e335f7bbb51
John Duong <jhnduong@gmail.com>
2020-04-02 12:09:16 -0700

    update review done

--- server/src/graphql/index.js
+++ server/src/graphql/index.js
@@ -1,9 +1,11 @@
 const Query = require('./Query')
 const Media = require('./resolvers/Media')
 const Review = require('./resolvers/Review')
+const Mutation = require('./Mutation')
 const resolvers = {
   Media,
   Query,
   Review,
+  Mutation
 }
 module.exports = { resolvers }
